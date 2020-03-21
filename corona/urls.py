3d22c5a0d6590a8951a6146eb45af25ac92d17f7
Digima Web Solutions <digimawebsolutions@gmail.com>
2020-03-21 22:46:35 +0800

    update settings

--- corona/urls.py
+++ corona/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from corona import views
+from .views import index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
 ]
