448e37e0cb198db74b7b201c056c2161f1a4ec1e
Digima Web Solutions <digimawebsolutions@gmail.com>
2020-03-21 22:57:53 +0800

    update settings

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',  # Enable the inner app
-    'corona'
+    'corona',
 ]
 
 MIDDLEWARE = [
--- corona/urls.py
+++ corona/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import index
+from corona.views import *
 
 urlpatterns = [
     path('', index, name='index'),
