28893c5409eb768a596f1afd77e9600eda02a56b
Digima Web Solutions <digimawebsolutions@gmail.com>
2020-03-27 21:59:56 +0800

    sitemap

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,21 @@ License: MIT
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.contrib import admin
+from django.contrib import admin, sitemaps
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include  # add this
+
+from corona.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap
+}
 
 
 urlpatterns = [
     path("", include("corona.urls")),
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps})
     # path("", include("authentication.urls")),  # add this
     # path("", include("app.urls"))  # add this
 ]
