f5ed52b51987b14953fce16d1d2b3dae92d98482
Digima Web Solutions <digimawebsolutions@gmail.com>
2020-04-13 14:02:20 +0800

    allowedhost

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,10 @@ STATICFILES_DIRS = (
 
 # Activate Django-Heroku.
 # django_heroku.settings(locals())
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
